e84e164ae73d89da9870a53234b0c8077f6c972d
Arsenii Kopaev <Senya.Kopaev@mail.ru>
2023-10-30 19:56:59 +0300

    refactored header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 /* VENDOR */
 import { useState } from "react";
-import { Link, useLocation } from "react-router-dom";
+import {Link, NavLink, useLocation} from "react-router-dom";
 
 /* APPLICATION */
 import "./Header.css";
@@ -9,41 +9,24 @@ import { ModalCreateItem } from "../Modal/ModalCreateItem";
 export const Header = () => {
   const { pathname } = useLocation(),
     isCategories = pathname.includes("categories"),
-    [createModalActive, setCreateModalActive] = useState(false);
+    [createModalActive, setCreateModalActive] = useState<boolean>(false);
+  const handleSwitchCreateModal = (e: React.MouseEvent<HTMLButtonElement>) => {
+      setCreateModalActive(prev => !prev)
+  }
 
   return (
     <header className="header">
       <h1 className="header-title">ToDo List</h1>
       <nav className="header-nav">
-        <ul className="header-list">
-          <li
-            className={
-              !isCategories
-                ? "header-list-item header-list-item-active"
-                : "header-list-item"
-            }
-          >
-            <Link to="tasks">Задачи</Link>
-          </li>
-          <li
-            className={
-              isCategories
-                ? "header-list-item header-list-item-active"
-                : "header-list-item"
-            }
-          >
-            <Link to="categories">Категории</Link>
-          </li>
-        </ul>
-        <button
-          className="header-button"
-          onClick={() => {
-            setCreateModalActive(true);
-          }}
-        >
-          {isCategories ? "Добавить категорию" : "Добавить задачу"}
-        </button>
+          <NavLink to={"/tasks"} className={({ isActive}) => isActive ? "header-nav-item header-nav-item-active" : "header-nav-item"}>Задачи</NavLink>
+          <NavLink to={"/categories"} className={({ isActive}) => isActive ? "header-nav-item header-nav-item-active" : "header-nav-item"}>Категории</NavLink>
       </nav>
+      <button
+        className="header-button"
+        onClick={handleSwitchCreateModal}
+      >
+        {isCategories ? "Добавить категорию" : "Добавить задачу"}
+      </button>
       <ModalCreateItem
         active={createModalActive}
         setActive={setCreateModalActive}
