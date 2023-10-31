d77f8e69e750cd3769857ce5c7f4d7b0a931407e
Arsenii Kopaev <Senya.Kopaev@mail.ru>
2023-10-31 11:13:53 +0300

    Made tasks filters + installed tailwind

--- src/components/Lists/Tasks.tsx
+++ src/components/Lists/Tasks.tsx
@@ -4,15 +4,69 @@ import { useSelector } from "react-redux"
 /* APPLICATION */
 import { ListItem } from "./ListItem"
 import { getAllTasks } from "../../features/tasksSlice"
+import React, { useState } from "react"
+import { Select } from "../../ui/Select/Select"
+import { Input } from "../../ui/Input/Input"
+
+import "../Modal/Modal.css"
 
 export const Tasks: React.FC = () => {
     const tasks = useSelector(getAllTasks)
 
+    const [search, setSearch] = useState<string>("")
+    const [category, setCategory] = useState<string>("")
+
+    const viewList = tasks.filter((task) => {
+        if (!category && !search) {
+            return true
+        }
+
+        if (category && search) {
+            return (
+                task.category === category &&
+                task.name.toLowerCase().includes(search.toLowerCase())
+            )
+        }
+
+        if (category) {
+            return task.category === category
+        }
+
+        if (search) {
+            return task.name.toLowerCase().includes(search.toLowerCase())
+        }
+
+        return false
+    })
+
+    const handleClearFilters = (e: React.MouseEvent<HTMLButtonElement>) => {
+        setSearch("")
+        setCategory("")
+    }
+
     return (
-        <ul>
-            {tasks.map((task) => (
-                <ListItem key={task.id} item={task} />
-            ))}
-        </ul>
+        <>
+            <div className={"flex"}>
+                <Input
+                    value={search}
+                    label={"Название задачи"}
+                    placeholder={"Введите назавние задачи"}
+                    setValue={setSearch}
+                />
+                <Select
+                    value={category}
+                    label={"Категория"}
+                    placeholder={"Введите назавние категории"}
+                    setValue={setCategory}
+                />
+                <button onClick={handleClearFilters}>Очистить</button>
+            </div>
+            <ul>
+                {viewList.map((task) => (
+                    <ListItem key={task.id} item={task} />
+                ))}
+                {viewList.length === 0 && <div>Ничего не найдено!</div>}
+            </ul>
+        </>
     )
 }
